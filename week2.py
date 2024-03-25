d5c525f9de6e3f1aa35a078d82d86da3e4e68061
Nomcebo-coder-hub <nmthembu2797@gmail.com>
2024-03-25 03:48:57 +0200

    Added extend into the my_list variable

--- week2.py
+++ week2.py
@@ -3,3 +3,13 @@ print(my_list)
 
 my_list.append(15)
 print("After Append:" , my_list)
+
+my_list = [10, 20, 30, 40]
+print("List1:" , my_list)
+
+even_numbers = [50, 60, 70]
+print("List2:" , even_numbers)
+
+my_list.extend(even_numbers)
+
+print("List after append:" , my_list)
